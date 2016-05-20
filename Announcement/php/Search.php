e10eccd73bac97f9986a8477e602fe78bff0bf47
tempurak <rak.s@mail.kmutt.ac.th>
2016-05-20 14:25:53 +0700

    Update Search.php

--- Announcement/php/Search.php
+++ Announcement/php/Search.php
@@ -1,9 +1,10 @@
 <?php
 	require_once('../../Database/Connection.php');
-
+	//connect database
 	$db = new database;
 	$db->connectdb();
-
+	
+	//create string for query
 	$Type = $_POST["Type"];
 	$Topic = $_POST["Topic"];
 	$StartFrom = $_POST["StartFrom"];
@@ -11,7 +12,7 @@
 	if($StartFrom != ""){$StartFrom = date('Y-m-d',strtotime($StartFrom));}
 	if($StartTo != ""){$StartTo = date('Y-m-d',strtotime($StartTo));}
 
-
+	//query data from database
 	$query = "SELECT * FROM Announcement ";
 	$query2 = "";
 	if( $Type != "" ){
@@ -29,7 +30,8 @@
 	
 	$query = $query.$query2;
 	$result = $db->querydb($query);
-
+	
+	//convert data to Table
 	$i = 1;
 	$data = "<tr class='text-bold'><td class = 'num'>ID</td><td>Topic</td><td>Type</td><td>Detail</td><td>Date</td><td>By</td></tr>";
 	while($row = mysqli_fetch_assoc($result)){
